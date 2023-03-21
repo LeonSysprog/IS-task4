ad5857a7f8b2250d73a6636611d67b31a68dbe57
LeonSysprog <91860867+LeonSysprog@users.noreply.github.com>
2023-03-21 13:03:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -25,21 +25,21 @@ const (
 )
 
 var authTokenString string
-var userName		string
+var userName	    string
 
 type jsonUser struct {
-    User		string `json:"usr"`
-    Password	string `json:"pwd"`
+    	User		string `json:"usr"`
+    	Password	string `json:"pwd"`
 }
 
 func main() {
-	/* server launch */
-    router := gin.Default()
-    router.POST("/registrate", postRegistrate)
-    router.POST("/auth", postAuth)
-    router.POST("/refresh", postRefresh)
-    router.Run("localhost:8080")
-    /*****************/
+    	/* server launch */
+    	router := gin.Default()
+    	router.POST("/registrate", postRegistrate)
+    	router.POST("/auth", postAuth)
+    	router.POST("/refresh", postRefresh)
+    	router.Run("localhost:8080")
+    	/*****************/
 }
 
 func setupDB() *sql.DB {
@@ -58,11 +58,11 @@ func cryptoRandInt() int64 {
 }
 
 func postRegistrate(c *gin.Context) {
-    var newJsonUser jsonUser
-
-    if err := c.BindJSON(&newJsonUser); err != nil {
-        return
-    }
+	var newJsonUser jsonUser
+	
+    	if err := c.BindJSON(&newJsonUser); err != nil {
+        	return
+    	}
 
 	salt := strconv.FormatInt(cryptoRandInt(), 10)
 	
@@ -75,33 +75,33 @@ func postRegistrate(c *gin.Context) {
 	db := setupDB()
 	db.QueryRow("INSERT INTO users(\"name\", \"salt\", \"hash\") VALUES($1, $2, $3);", newJsonUser.User, salt, hashPasswordSalt)
 	
-    c.IndentedJSON(http.StatusOK, newJsonUser)
+    	c.IndentedJSON(http.StatusOK, newJsonUser)
 }
 
 func postAuth(c *gin.Context) {
 	var authJsonUser jsonUser
 
-    if err := c.BindJSON(&authJsonUser); err != nil {
-        return
-    }
+    	if err := c.BindJSON(&authJsonUser); err != nil {
+        	return
+    	}
     
-    db := setupDB()
-    row := db.QueryRow("SELECT * FROM users WHERE name = $1;", authJsonUser.User)
-    fmt.Println(authJsonUser.User)
+    	db := setupDB()
+    	row := db.QueryRow("SELECT * FROM users WHERE name = $1;", authJsonUser.User)
+    	fmt.Println(authJsonUser.User)
     
-    if row == nil {
-    	c.IndentedJSON(http.StatusForbidden, authJsonUser)
-    }
+    	if row == nil {
+    		c.IndentedJSON(http.StatusForbidden, authJsonUser)
+    	}
     	
-    var id 	 int
-    var user string
-    var salt string
-    var hash string
+    	var id 	 int
+    	var user string
+    	var salt string
+    	var hash string
     	
-    row.Scan(&id, &user, &salt, &hash)
+    	row.Scan(&id, &user, &salt, &hash)
     	
-    hashFunction := sha256.New()
-    passwordSalt := authJsonUser.Password + salt
+    	hashFunction := sha256.New()
+    	passwordSalt := authJsonUser.Password + salt
 	
 	hashFunction.Write([]byte(passwordSalt))
 	hashPasswordSalt := hex.EncodeToString(hashFunction.Sum(nil))
@@ -121,19 +121,19 @@ func postRefresh(c *gin.Context) {
 	var tokenString string
 	
 	if err := c.BindJSON(&tokenString); err != nil {
-        return
-    }
+        	return
+    	}
     
-    if tokenString == authTokenString {
-    	c.SetCookie("Request-Token", tokenString, 3660, "/", "", true, true)
+    	if tokenString == authTokenString {
+    		c.SetCookie("Request-Token", tokenString, 3660, "/", "", true, true)
     	
-    	tokenString := createToken(2)
+    		tokenString := createToken(2)
     	
-    	c.SetCookie("Access-Token", tokenString, 7320, "/", "", true, true)
+    		c.SetCookie("Access-Token", tokenString, 7320, "/", "", true, true)
 		c.IndentedJSON(http.StatusOK, tokenString)
-    } else {
-    	c.IndentedJSON(http.StatusForbidden, tokenString)
-    }
+    	} else {
+    		c.IndentedJSON(http.StatusForbidden, tokenString)
+    	}
 }
 
 func createToken(hours time.Duration) string {
